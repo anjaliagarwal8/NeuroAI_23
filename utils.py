197e9080bcb29990642cad617edb7e619726c8ca
Domenick Mifsud <47504507+domenick-m@users.noreply.github.com>
2023-07-11 23:02:44 -0400

    update

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def assert_gpu_runtime():
-    error_message = '\n\n⚠️ ERROR: GPU is not detected! ⚠️\n\nPlease change the runtime type via:\n\tSelect "Runtime" > "Change runtime type" > "Hardware accelerator" > "GPU"\nand re-run the notebook.'
+    error_message = '\n\n⚠️ ERROR: GPU is not detected! ⚠️\n\nPlease change the runtime type and re-run the notebook. To do so:\n\tSelect "Runtime" > "Change runtime type" > "Hardware accelerator" > "GPU"'
     sp_call = subprocess.call('nvidia-smi', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     # run `nvidia-smi` to see if we can see the GPU, if not raise AssertionError
     assert  sp_call == 0, f'{error_message}'
